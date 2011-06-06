2c39c2834a7c3804b0c04d4c5da947a53437d36c
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-06-06 21:32:28 +0000

    Hack updated PHP package to work on RHEL6 where the php package by default is 5.3

--- recipes/package.rb
+++ recipes/package.rb
@@ -20,7 +20,7 @@
 
 pkgs = value_for_platform(
   [ "centos", "redhat", "fedora" ] => {
-    "default" => %w{ php53 php53-devel php53-cli php-pear }
+    "default" => %w{ php php-devel php-cli php-pear }
   },
   [ "debian", "ubuntu" ] => {
     "default" => %w{ php5-cgi php5 php5-dev php5-cli php-pear }

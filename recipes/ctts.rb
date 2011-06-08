3f346e9282749e9ce8507b690aae09bcc6045d93
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-06-08 03:25:27 +0000

    Fix custom PHP ini install path.

--- recipes/ctts.rb
+++ recipes/ctts.rb
@@ -7,12 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-ini_path = value_for_platform({
-  ["centos", "redhat", "fedora", "suse"] => { "default" => "/etc/php.d/ctts.ini" },
-  "default" => "/etc/php5/conf.d/ctts.ini",
-})
-
-cookbook_file "#{node[:php][:confd_path]}/ctts.ini" do
+cookbook_file "#{node[:php][:ext_conf_dir]}/ctts.ini" do
   source "ctts.ini"
   owner "root"
   group "root"
